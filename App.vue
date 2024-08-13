aaf2688d86f1730642a6ad38b86358671db4d737
Veta2000 <elizter121@gmail.com>
2024-08-13 05:13:03 +0300

    changes

---
+++ App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <h2>{{ message }}</h2>
+      <p>Counter: {{ counter }}</p>
+      <button @click="counter++">Увеличить значение</button>
+      
+      
+      <input type="text" v-model="inputText" placeholder="Текст">
+      <p>Input Text: {{ inputText }}</p>
+      
+   
+      <GreetingComponent />
+      <MessageComponent />
+    </div>
+  </template>
+  <script>
+  
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: ' Привет!',
+      counter: 0,
+      inputText: ''
+    };
+}
+  };
+  </script>
+
+  <style>
+  h2 {
+    color: red;
+  }
+  </style>
+  
---
+++ components/GreetingComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h1>HELLO</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'GreetingComponent'
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: blue;
+  }
+  </style>
+  
